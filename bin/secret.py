f99b73549dd7f96bf0f8f772f8bf231c52dc2933
Barry Collins <collibar@gmail.com>
2020-04-02 16:29:10 +0100

    Delete secret.py

--- bin/secret.py
+++
@@ -1,6 +0,0 @@
-# rename this file to `secret.py`
-USERNAME = 'btdcollins@outlook.com'
-PASSWORD = 'D3c3pt10n!'
-#NS_URL = 'http://localhost/'
-NS_URL = "https://komplex-nightscout.herokuapp.com/"
-NS_SECRET = 's9FvxlhhEDdsBTjxqfZD'
